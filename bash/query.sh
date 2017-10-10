a0bed74fa59c5f2bb220e8d33e8271f5f68ec881
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-10-10 19:20:24 +0000

    Update query.sh

--- bash/query.sh
+++ bash/query.sh
@@ -1,1 +1,67 @@
 #!/bin/bash
+php -q /hostdata/api.dopehosting.net/whmcs/clients.php > /hostdata/api.dopehosting.net/public_html/whmcs/json/clients.json
+php -q /hostdata/api.dopehosting.net/whmcs/host.php > /hostdata/api.dopehosting.net/public_html/whmcs/json/host.json
+php -q /hostdata/api.dopehosting.net/whmcs/domains.php > /hostdata/api.dopehosting.net/public_html/whmcs/json/domains.json
+php -q /hostdata/api.dopehosting.net/namecheap/get_asia.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/asia.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_cc.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/cc.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_club.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/club.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_co.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/co.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_com.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/com.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_email.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/email.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_host.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/host.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_in.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/in.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_io.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/io.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_life.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/life.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_live.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/live.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_me.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/me.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_net.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/net.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_online.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/online.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_org.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/org.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_pw.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/pw.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_review.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/review.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_sh.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/sh.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_space.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/space.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_store.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/store.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_stream.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/stream.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_tech.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/tech.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_tokyo.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/tokyo.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_trade.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/trade.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_tv.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/tv.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_us.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/us.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_win.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/win.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_world.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/world.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_xyz.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/xyz.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_zone.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/zone.json
+sleep 2
+php -q /hostdata/api.dopehosting.net/namecheap/get_blog.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/blog.json
+sleep 1
+php -q /hostdata/api.dopehosting.net/namecheap/get_news.php > /hostdata/api.dopehosting.net/public_html/namecheap/json/news.json
